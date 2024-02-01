4c1d0bfd259b05b49d5fbf639ee1116d2698bc83
yyngfive <chenhye5@outlook.com>
2024-02-01 17:32:03 +0800

    web analysis

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,8 @@ import "./globals.css";
 import { Providers } from "./providers";
 import NavBar from '@/components/NavBar';
 import Footer from "@/components/Footer";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: "XNA Tools",
@@ -14,15 +15,15 @@ export default function RootLayout({ children }) {
     <html lang="en" className="light">
       <body >
         <Providers>
-          <NavBar/>
+          <NavBar />
           <div className="container mx-auto min-h-full relative pb-20">
-          {children}
-          
+            {children}
+
           </div>
-          <Footer/>
+          <Footer />
         </Providers>
         <SpeedInsights />
-
+        <Analytics />
       </body>
     </html>
   );

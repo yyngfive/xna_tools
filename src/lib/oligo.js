aa811efb1693fa691eea1bbb75acf2d96e95d6cb
yyngfive <chenhye5@outlook.com>
2024-02-01 22:09:08 +0800

    增加DNA消光系数的计算，RNA的可能有数值错误

--- src/lib/oligo.js
+++ src/lib/oligo.js
@@ -45,8 +45,26 @@ export function sequence_complement(sequence) {
                 return match;
         }
     });
-    const complement = res.split('').reverse().join('')
+    const complement = res.split('').reverse().join('');
     return (complement);
+}
+
+function base_count(sequence_cleaned) {
+    const An = (sequence_cleaned.match(/A/g) || []).length;
+    const Cn = (sequence_cleaned.match(/C/g) || []).length;
+    const Gn = (sequence_cleaned.match(/G/g) || []).length;
+    const Tn = (sequence_cleaned.match(/T/g) || []).length;
+    const Un = (sequence_cleaned.match(/U/g) || []).length;
+    const In = (sequence_cleaned.match(/I/g) || []).length;
+
+    return ({
+        An,
+        Cn,
+        Gn,
+        Tn,
+        Un,
+        In,
+    });
 }
 
 export function sequence_value(sequence, type) {
@@ -58,35 +76,110 @@ export function sequence_value(sequence, type) {
         weight: 0,
     };
 
-    const dA = 313.209;
-    const dC = 289.184;
-    const dG = 329.208;
-    const dT = 304.196;
-    const dI = 314.194;
-    const dU = 290.169;
+    const MW = {
+        dA: 313.209,
+        dC: 289.184,
+        dG: 329.208,
+        dT: 304.196,
+        dI: 314.194,
+        dU: 290,
+        O: 15.999,
+        PO2H: 63.980,
+        H2: 2.016,
+    };
 
-    const O = 15.999;
-    const PO2H = 63.980;
-    const H2 = 2.016;
+    const Ext_DNA = {
+        A: 15400,
+        C: 7400,
+        G: 11500,
+        T: 8700,
+        AA: 27400,
+        AC: 21200,
+        AG: 25000,
+        AT: 22800,
+        CA: 21200,
+        CC: 14600,
+        CG: 18000,
+        CT: 15200,
+        GA: 25200,
+        GC: 17600,
+        GG: 21600,
+        GT: 20000,
+        TA: 23400,
+        TC: 16200,
+        TG: 19000,
+        TT: 16800,
+    };
+
+    //BUG 数据可能有问题
+    const Ext_RNA = {
+        A: 15400,
+        C: 7400,
+        G: 11500,
+        U: 9900,
+        AA: 27400,
+        AC: 21000,
+        AG: 25000,
+        AU: 24000,
+        CA: 21000,
+        CC: 14200,
+        CG: 17800,
+        CU: 16200,
+        GA: 25200,
+        GC: 17400,
+        GG: 21600,
+        GU: 21200,
+        UA: 24600,
+        UC: 17200,
+        UG: 20000,
+        UU: 19600,
+    };
 
     const sequence_cleaned = sequence_clean(sequence);
 
-    const An = (sequence_cleaned.match(/A/g) || []).length;
-    const Cn = (sequence_cleaned.match(/C/g) || []).length;
-    const Gn = (sequence_cleaned.match(/G/g) || []).length;
+
     //TODO 考虑RNA修饰的分子量，对每个r标记增加一个氧原子分子量
-    if (type === 'DNA') {
-        const Tn = (sequence_cleaned.match(/T/g) || []).length;
-        //const weight = An * 331.23 + Tn * 322.22 + Cn * 307.20 + Gn * 347.23 - 18.02 * (sequence_cleaned.length - 1) - 79.98;
-        const weight = An * dA + Tn * dT + Cn * dC + Gn * dG - PO2H + H2;
-        oligo_value.weight = weight.toFixed(1);
-    } else if (type === 'RNA') {
-        const Un = (sequence_cleaned.match(/U/g) || []).length;
-        const weight = An * dA + Un * dU + Cn * dC + Gn * dG - PO2H + H2 + sequence_cleaned.length * O;
-        oligo_value.weight = weight.toFixed(1);
+
+    const pairs = [];
+
+    for (let i = 0; i < sequence_cleaned.length - 1; i += 1) {
+        const pair = sequence_cleaned.slice(i, i + 2);
+        pairs.push(pair);
     }
-    return (sequence_cleaned != ''? oligo_value:{weight:0,tm:0,ext:0});
+
+    if (type === 'DNA') {
+        const { An, Cn, Gn, Tn } = base_count(sequence_cleaned);
+        //const weight = An * 331.23 + Tn * 322.22 + Cn * 307.20 + Gn * 347.23 - 18.02 * (sequence_cleaned.length - 1) - 79.98;
+        const weight = An * MW.dA + Tn * MW.dT + Cn * MW.dC + Gn * MW.dG - MW.PO2H + MW.H2;
+        oligo_value.weight = weight.toFixed(1);
+
+        const { An:Ai, Cn:Ci, Gn:Gi, Tn:Ti } = base_count(sequence_cleaned.slice(1, sequence_cleaned.length - 1));
+        //console.log(sequence_cleaned.slice(1, sequence_cleaned.length - 1));
+        let ext = 0;
+        pairs.forEach(e => {
+            //console.log(e,typeof Ext_DNA[e])
+            ext += Ext_DNA[e];
+        });
+        console.log(Ai);
+        ext = ext - (Ai * Ext_DNA.A + Ci * Ext_DNA.C + Ti * Ext_DNA.T + Gi * Ext_DNA.G);
+        oligo_value.ext = ext;
+
+    } else if (type === 'RNA') {
+        const { An, Cn, Gn, Un } = base_count(sequence_cleaned);
+        const weight = An * MW.dA + Un * MW.dU + Cn * MW.dC + Gn * MW.dG - MW.PO2H + MW.H2 + sequence_cleaned.length * MW.O;
+        oligo_value.weight = weight.toFixed(1);
+
+        const { An:Ai, Cn:Ci, Gn:Gi, Un:Ui } = base_count(sequence_cleaned.slice(1, sequence_cleaned.length - 1));
+        let ext = 0;
+        pairs.forEach(e => {
+            ext += Ext_RNA[e];
+        });
+        ext = ext - (Ai * Ext_RNA.A + Ci * Ext_RNA.C + Ui * Ext_RNA.U + Gi * Ext_RNA.G);
+        oligo_value.ext = ext;
+
+    }
+    return (sequence_cleaned != '' ? oligo_value : { weight: 0, tm: 0, ext: 0 });
 }
 
-let test_seq = 'uUGACGuACuuGCAACAGAGAGCuuu';
-console.log(sequence_verify(test_seq, 'RNA'), sequence_counts(test_seq), sequence_value(test_seq, 'RNA'));
+let test_seq = 'ACG TGA TCG ATC TCG ATT T';
+console.log(sequence_verify(test_seq, 'DNA'), sequence_counts(test_seq), sequence_value(test_seq, 'DNA'));

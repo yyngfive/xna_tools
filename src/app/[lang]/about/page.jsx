bd3e957d43ab6fd719dfd5905cecaf5e989896e8
yyngfive <chenhye5@outlook.com>
2024-06-23 21:12:21 +0800

    更新了关于

--- src/app/[lang]/about/page.jsx
+++ src/app/[lang]/about/page.jsx
@@ -2,10 +2,9 @@
 export default function About(){
     return(
         <div className="w-full text-center my-6 leading-relaxed">
-            <p>Frame: Next.JS</p>
+            <p>Framework: Next.JS</p>
             <p>UI: NextUI</p>
             <p>Docs: MDX</p>
-            <p>Deployed: Vercel</p>
         </div>
     );
 }
